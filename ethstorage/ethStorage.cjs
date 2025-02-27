e4b6b92511b09bd162115c6fe13520a43da8dabc
William Wang <bwilliamwang@gmail.com>
2025-02-26 21:20:22 -0700

    Update ethStorage.cjs

--- ethstorage/ethStorage.cjs
+++ ethstorage/ethStorage.cjs
@@ -1,36 +1,49 @@
-require('dotenv').config({ path: '.env' });
+require("dotenv").config({ path: ".env" });
 
 const { FlatDirectory } = require("ethstorage-sdk");
 
 const privateKey = process.env.ETHSTORAGE_PRIVATE_KEY;
 const rpc = "https://rpc.beta.testnet.l2.quarkchain.io:8545";
 
+async function test() {
+  const address = "0xA460C70b474cA4125c35dFaFfC1e83B0122efcaB"; // FlatDirectory address
 
-async function test()  {
-    const address = "0xA460C70b474cA4125c35dFaFfC1e83B0122efcaB"; // FlatDirectory address
+  const flatDirectory = await FlatDirectory.create({
+    rpc: rpc,
+    privateKey: privateKey,
+    address: address,
+  });
 
-    const flatDirectory = await FlatDirectory.create({
-        rpc: rpc,
-        privateKey: privateKey,
-        address: address,
-    });
-
+  if (false) {
     // await flatDirectory.deploy();
-    console.log("flat directory successful", flatDirectory)
+    console.log("flat directory successful", flatDirectory);
     const request = {
-        key: "test.txt",
-        content: Buffer.from("big data"),
-        type: 2, // 1 for calldata and 2 for blob
-        callback: {
-            onProgress(progress, count, isChange) {
-            },
-            onFail(err) {
-            },
-            onFinish(totalUploadChunks, totalUploadSize, totalStorageCost) {
-            }
-        }
-    }
+      key: "test.txt",
+      content: Buffer.from("big data"),
+      type: 2, // 1 for calldata and 2 for blob
+      callback: {
+        onProgress(progress, count, isChange) {},
+        onFail(err) {},
+        onFinish(totalUploadChunks, totalUploadSize, totalStorageCost) {},
+      },
+    };
     const response = await flatDirectory.upload(request);
-    console.log("uploaded", response)
+    console.log("uploaded", response);
+  }
+
+  const key = "test.txt";
+  await flatDirectory.download(key, {
+    onProgress: function (progress, count, chunk) {
+      console.log(
+        `Download ${progress} of ${count} chunks, this chunk is ${chunk.toString()}`
+      );
+    },
+    onFail: function (error) {
+      console.error("Error download data:", error);
+    },
+    onFinish: function () {
+      console.log("Download success.");
+    },
+  });
 }
 test();
